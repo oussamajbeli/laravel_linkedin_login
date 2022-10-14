f3db18daca3ee9826a70d7b27ba5cc5f10b3a25c
unknown <oussamajbeliiset@gmail.com>
2022-10-14 08:24:30 +0100

    first commit

---
+++ app/Http/Controllers/LinkedinController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Socialite;
+use Exception;
+use App\Models\User;
+class LinkedinController extends Controller
+{
+    public function linkedinRedirect(){
+        return Socialite::driver('linkedin')->redirect();
+    }
+
+    public function linkedinCallback(){
+        try{
+            $user = Socialite::driver('linkedin')->user();
+            $linkedinUser = User::where('oauth_id',$user->id)->first();
+            if($linkedinUser){
+                Auth::login($linkedinUser);
+                return redirect()->route('home'); 
+            }else{
+                $user = User::create([
+                    'name'=>$user->name,
+                    'email'=>$user->email,
+                    'oauth_id'=>$user->id,
+                    'oauth_type'=>'linkedin',
+                    'password'=>encrypt('admin12345'),
+                ]);
+                Auth::login($user);
+                return redirect('home');
+            }
+        }catch(Exception $e){
+            dd($e->getMessage());
+        }
+    }
+}
